9ab4a7d58038196f0ff7419233aee29debab1ffa
Chirag-Davda <dchirag433@gail.com>
2024-09-16 12:04:51 +0530

    Initial commit

--- Book/models.py
+++ Book/models.py
@@ -6,6 +6,9 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField()
     
+    def __str__(self):
+        return self.name
+    
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -13,14 +16,23 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     
+    def __str__(self):
+        return self.title
+       
 class Publisher(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
     books = models.ManyToManyField(Book,related_name='publisher')
     
+    def __str__(self):
+        return self.name
+    
 class Review(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE,related_name='reviews')
     rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])  # 1 to 5
     comment = models.TextField()
+
+    def __str__(self):
+        return f"Review for {self.book.title} - {self.rating}/5"
     
     
--- Book/views.py
+++ Book/views.py
@@ -1,19 +1,29 @@
 from rest_framework import viewsets
 from .models import Author, Book, Publisher, Review
 from .serializers import AuthorSerializer, BookSerializer, PublisherSerializer, ReviewSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class PublisherViewSet(viewsets.ModelViewSet):
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
